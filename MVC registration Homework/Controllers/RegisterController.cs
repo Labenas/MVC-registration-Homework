3245bfc8ab1036e13757df0f04349be44d47b099
Labenas <66065598+Labenas@users.noreply.github.com>
2020-12-22 17:55:17 +0200

    Add project files.

---
+++ MVC registration Homework/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_registration_Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_registration_Homework.Controllers
+{
+    public class RegisterController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RegisterModel model)
+        {
+            return View("Index",model);
+        }
+
+        
+    }
+}
---
+++ MVC registration Homework/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_registration_Homework.Models
+{
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
